85a8270daff8800dfba7d46500eb66e495b13d64
Holger Brandl <holgerbrandl@gmail.com>
2015-10-06 11:24:31 +0200

    continued toy example

--- src/main/scala/scalautils/SimpleApp.scala
+++ src/main/scala/scalautils/SimpleApp.scala
@@ -2,13 +2,18 @@ package scalautils
 
 /**
  * Document me!
- *
  * @author Holger Brandl
  */
 // note: The App trait can be used to quickly turn objects into executable programs. Here is an example:
-// "object" serves the same (and some additional) purposes as the static methods and fields in Java
-object SimpleApp extends App{
-  println("test")
 
-  def doSomething = println("something")
+// http://stackoverflow.com/questions/1755345/difference-between-object-and-class-in-scala
+// "object" serves the same (and some additional) purposes as the static methods and fields in Java
+// Scala doesn't have static methods or fields. Instead you should use object
+
+object SimpleApp extends App{
+
+  println("initialize me!")
+
+
+  def doSomething = println("something else")
 }
