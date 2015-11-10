1e130527ec603aedbe517d4eadb80917fb371494
Holger Brandl <holgerbrandl@gmail.com>
2015-11-10 11:09:39 +0100

    cosmetics

--- src/test/scala/FastaTest.scala
+++ src/test/scala/FastaTest.scala
@@ -1,35 +1,27 @@
 import better.files.File
 import org.scalatest.{FlatSpec, Matchers}
 
-/**
-  * Document me!
-  *
-  * @author Holger Brandl
-  */
+import scalautils.FastaUtils._
+
 class FastaTest extends FlatSpec with Matchers {
+
+  // see http://www.scalatest.org/user_guide/using_matchers#greaterAndLessThan
 
   it should "create some fasta chunks from a source fasta " in {
     //  import better.files._, Cmds._
 
-    val testWD = File("/Volumes/home/brandl/test/")
+    val wd = File("/Volumes/home/brandl/test/")
     //   val sourceExample = File("/projects/plantx/on_plantx/dd_Jani_v4/dd_Jani_v4.dircor.fasta")
-    //   val testFasta = sourceExample.copyTo(testWD/"some_seqs.fasta")
-    val testFasta = testWD / "some_seqs.fasta"
+    //   val testFasta = sourceExample.copyTo(wd/"some_seqs.fasta")
+    val testFasta = wd / "some_seqs.fasta"
 
-
-    //  "cp /projects/plantx/on_plantx/dd_Jani_v4/dd_Jani_v4.dircor.fasta /Volumes/home/brandl/test/"
-    //  cp(sourceExample, testWD)
-
-    testWD.list
-
-    import scalautils.FastaUtils._
 
     val fastaRecords = readFasta(testFasta)
 
-    createChunks(openFasta(testFasta), 20, new SimpleChunkNamer(baseDir = testWD, prefix = "test_chunk_"))
+    createChunks(openFasta(testFasta), 20, new SimpleChunkNamer(baseDir = wd, prefix = "test_chunk_"))
 
 
-    testWD.glob("**/test_chunk*").size should be > 0
+    wd.glob("**/test_chunk*").size should be > 0
   }
 
   it should "do something else " in {
