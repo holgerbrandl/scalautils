0dfd890947496e7bd92fa94d0464604b29063281
Holger Brandl <holgerbrandl@gmail.com>
2015-11-16 14:08:55 +0100

    migrated joblist into own repository

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 
 
 name := "scalautils"
-
+organization := "de.mpicbg.scicomp"
 version := "0.1-SNAPSHOT"
 
 scalaVersion := "2.11.7"
--- src/main/scala/scalautils/IOUtils.scala
+++ src/main/scala/scalautils/IOUtils.scala
@@ -1,6 +1,6 @@
 package scalautils
 
-import java.io.File
+import java.io.{File, PrintWriter}
 
 import scala.language.implicitConversions
 
@@ -14,16 +14,16 @@ object IOUtils {
 
   // for partially applied functions see
   // http://stackoverflow.com/questions/14309501/scala-currying-vs-partially-applied-functions
-  def saveAs(fileName: String) = saveAs(new File(fileName)) _
+  def saveAs(fileName: String): ((PrintWriter) => Unit) => Unit = saveAs(new File(fileName))
 
 
-  def saveAs(file: better.files.File) = saveAs(file.toJava) _
+  def saveAs(file: better.files.File): ((PrintWriter) => Unit) => Unit = saveAs(file.toJava)
 
   // see http://stackoverflow.com/questions/4604237/how-to-write-to-a-file-in-scala
   //  http://stackoverflow.com/questions/6879427/scala-write-string-to-file-in-one-statement
   // similar http://jesseeichar.github.io/scala-io-doc/0.2.0/index.html#!/core/multiple_writes_single_connection
 
-  def saveAs(f: java.io.File)(op: java.io.PrintWriter => Unit, overwrite: Boolean = false) {
+  def saveAs(f: java.io.File, overwrite: Boolean = false)(op: java.io.PrintWriter => Unit) {
     if (f.isFile && !overwrite) throw new IllegalArgumentException(s"$f is present already")
     val p = new java.io.PrintWriter(f)
     try {
