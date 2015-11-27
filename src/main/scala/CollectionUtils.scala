f3f33236f804c4e59aac292b6844d66be8294f04
Holger Brandl <holgerbrandl@gmail.com>
2015-11-27 13:24:06 +0100

    implemnted strict set operations for better type safety

---
+++ src/main/scala/CollectionUtils.scala
@@ -0,0 +1,25 @@
+
+
+/**
+  * @author Holger Brandl
+  */
+object CollectionUtils {
+
+  implicit class StrictSetOps[T](someSeq: Seq[T]) {
+
+    def strictDiff(that: Seq[T]) = {
+      someSeq.diff(that).seq
+    }
+
+
+    def strictUnion(that: Seq[T]) = {
+      someSeq.union(that)
+    }
+
+
+    def strictIntersect(that: Seq[T]) = {
+      someSeq.intersect(that)
+    }
+  }
+
+}
