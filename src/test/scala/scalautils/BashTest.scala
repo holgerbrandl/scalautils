3e28019e264964a8855b934bfd4bd891025f41a3
Holger Brandl <holgerbrandl@gmail.com>
2015-12-06 12:05:58 +0100

    separated scheduler interface from abstract implementation

--- src/test/scala/scalautils/BashTest.scala
+++ src/test/scala/scalautils/BashTest.scala
@@ -1,5 +1,9 @@
 package scalautils
 
+import better.files.File
+
+import scalautils.Bash._
+
 /**
   * @author Holger Brandl
   */
@@ -14,4 +18,36 @@ object BashTest extends App {
 
   val jobCmed = "cd '/home/brandl/unit_tests'; mysub \"Users__brandl__633224592__1112201512d4102\" 'cd /home/brandl/unit_tests ## lsfutils:change into wd\nsleep 60; echo \"this is task 1\" > task_1.txt ' -q short   | joblist /home/brandl/unit_tests/.test_tasks"
   Bash.eval(jobCmed)
+}
+
+
+object BashPlayground {
+
+  import scala.language.postfixOps
+  import sys.process._
+
+  //http://oldfashi\onedsoftware.com/2009/07/10/scala-code-review-foldleft-and-foldright/
+  List("/bin/bash", "-c", s"'kaka'").foldLeft("")((b, a) => b + " " + a).trim
+
+
+  def R(rcmd: String) {
+    Seq("/bin/bash", "-c", s"echo '$rcmd' | Rscript --vanilla -") !
+  }
+
+
+  //http://docs.scala-lang.org/tutorials/tour/operators.html
+  //Any method which takes a single parameter can be used as an infix operator in Scala. x
+  //R "1+1"
+  R("1+1")
+
+  File("/home/brandl/.bash_profile") // .head
+
+  //import scala.sys.process._
+  //val cmd = "uname -a" // Your command
+  //val output = cmd.!!.trim // Captures the output
+
+  // or
+  // Process("cat temp.txt")!
+
+  eval("om $(pwd)")
 }
