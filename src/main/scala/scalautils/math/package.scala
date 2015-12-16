a8d42c0055c729b7ab7da2f7ad12a46b2e796926
Holger Brandl <holgerbrandl@gmail.com>
2015-12-16 09:03:30 +0100

    moved math helpers to scalautils

---
+++ src/main/scala/scalautils/math/package.scala
@@ -0,0 +1,22 @@
+package scalautils
+
+/**
+  * @author Holger Brandl
+  */
+package object math {
+
+  // http://stackoverflow.com/questions/4753629/how-do-i-make-a-class-generic-for-all-numeric-types
+  // http://stackoverflow.com/questions/3498784/scala-calculate-average-of-someobj-double-in-a-listsomeobj/34196631#34196631
+  implicit class ImplDoubleVecUtils(values: Seq[Double]) {
+
+    def mean = values.sum / values.length
+
+
+    // http://stackoverflow.com/questions/4662292/scala-median-implementation
+    def median = {
+      val (lower, upper) = values.sorted.splitAt(values.size / 2)
+      if (values.size % 2 == 0) (lower.last + upper.head) / 2.0 else upper.head
+    }
+  }
+
+}
