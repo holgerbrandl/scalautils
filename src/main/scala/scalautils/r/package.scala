9102ab42db93e654071a07f85983f59299b7f9f3
Holger Brandl <holgerbrandl@gmail.com>
2016-05-03 09:51:29 +0200

    use bootstrap method for report rendering

--- src/main/scala/scalautils/r/package.scala
+++ src/main/scala/scalautils/r/package.scala
@@ -2,21 +2,37 @@ package scalautils
 
 import better.files.File
 
+import scala.io.Source
+
 /**
   * @author Holger Brandl
   */
 package object r {
 
-  /**
-    *
-    * @return The create html report file
-    */
+  //todo port back into scalautils
   def rendrSnippet(reportName: String, rSnippet: String,
                    showCode: Boolean = true, args: String = "", wd: File = File(".")) = {
 
 
     val reportFileName = reportName.replaceAll("[/ ]", "_")
-    require(ShellUtils.isInPath("rend.R"), "rend.R is not installed. See https://github.com/holgerbrandl/datautils/tree/master/R/rendr")
+    require(ShellUtils.isInPath("R"), "Can not render report because R is not available on your system.")
+
+    //    require(ShellUtils.isInPath("rend.R"), "rend.R is not installed. See https://github.com/holgerbrandl/datautils/tree/master/R/rendr")
+
+    // bootstrap rend.R if it is not yet installed
+    val rendr2use = if(!ShellUtils.isInPath("rend.R")){
+      val rendR: File = File.home/".jl_rend.R"
+
+      if(!rendR.exists) {
+        val rendrURL: String = "https://raw.githubusercontent.com/holgerbrandl/datautils/v1.23/R/rendr/rend.R"
+        IOUtils.saveAs(rendR.toJava, Source.fromURL(rendrURL).getLines().toSeq)
+        rendR.toJava.setExecutable(true)
+      }
+
+      rendR.path.toAbsolutePath
+    }else{
+      "rend.R"
+    }
 
     val tempDir = File.newTemporaryDirectory(prefix = "rsnip_")
 
@@ -24,7 +40,7 @@ package object r {
 
     tmpR.write(rSnippet)
 
-    Bash.eval(s"""cd "${wd.path}"; rend.R ${if (showCode) "" else "-e"} "${tmpR.path}" $args""")
+    Bash.eval(s"""cd "${wd.path}"; ${rendr2use} ${if (showCode) "" else "-e"} "${tmpR.path}" $args""")
 
     // todo convert result into status code
     wd / (reportFileName + ".html")
